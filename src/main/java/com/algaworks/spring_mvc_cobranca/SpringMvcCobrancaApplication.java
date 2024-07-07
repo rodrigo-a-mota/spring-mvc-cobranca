386ddf4e456baa4545b0835fe04d2294616654f8
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-07-07 17:35:26 -0300

    Criado a estrutura do projeto

---
+++ src/main/java/com/algaworks/spring_mvc_cobranca/SpringMvcCobrancaApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.spring_mvc_cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcCobrancaApplication.class, args);
+	}
+
+}
