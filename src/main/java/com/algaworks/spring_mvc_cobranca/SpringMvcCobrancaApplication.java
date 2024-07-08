8337734cf758140409818a9b1b6f0b1c22c1f6cb
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-07-08 13:42:49 -0300

    Configurado o Locale e a formatação da data e valor

--- src/main/java/com/algaworks/spring_mvc_cobranca/SpringMvcCobrancaApplication.java
+++ src/main/java/com/algaworks/spring_mvc_cobranca/SpringMvcCobrancaApplication.java
@@ -2,12 +2,22 @@ package com.algaworks.spring_mvc_cobranca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.FixedLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringMvcCobrancaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcCobrancaApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 
 }
--- src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
@@ -1,6 +1,8 @@
 package com.algaworks.spring_mvc_cobranca.controller;
 
 import com.algaworks.spring_mvc_cobranca.model.Titulo;
+import com.algaworks.spring_mvc_cobranca.repository.Titulos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
+
+    @Autowired
+    private Titulos titulos;
 
     @RequestMapping("/novo")
     public String novo() {
@@ -17,8 +22,7 @@ public class TituloController {
     @RequestMapping(method = RequestMethod.POST)
     public String salvar(Titulo titulo) {
 
-
-
+        titulos.save(titulo);
         return "CadastroTitulo";
     }
 }
