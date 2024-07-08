7129ec351ef6cd950c6d9558f8fb23f4898dae4b
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-07-07 22:54:00 -0300

    Criado a conexão da view com o controller e criado a entidade título

--- src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
@@ -1,13 +1,24 @@
 package com.algaworks.spring_mvc_cobranca.controller;
 
+import com.algaworks.spring_mvc_cobranca.model.Titulo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 
-    @RequestMapping("/titulos/novo")
+    @RequestMapping("/novo")
     public String novo() {
+        return "CadastroTitulo";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String salvar(Titulo titulo) {
+
+        System.out.println(">>>>> " + titulo.getDescricao());
+
         return "CadastroTitulo";
     }
 }
