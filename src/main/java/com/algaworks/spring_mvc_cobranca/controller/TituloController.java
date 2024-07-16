95ae735d1138ab929934162403663ed8b23ef3f1
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-07-16 08:57:35 -0300

    Adicionado tags do thymeleaf para mostrar a menssagem de sucesso

--- src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/titulos")
@@ -20,9 +21,11 @@ public class TituloController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(Titulo titulo) {
-
+    public ModelAndView salvar(Titulo titulo) {
         titulos.save(titulo);
-        return "CadastroTitulo";
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("mensagem", "Título salvo com sucesso!");
+
+        return mv;
     }
 }
