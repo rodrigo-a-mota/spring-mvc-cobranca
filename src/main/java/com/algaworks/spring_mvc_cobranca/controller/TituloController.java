06b7e51d96ee1cc806a0f766093e32be9b1d7414
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-07-17 11:22:25 -0300

    Adicinando status na view dinamicamente

--- src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/spring_mvc_cobranca/controller/TituloController.java
@@ -1,12 +1,17 @@
 package com.algaworks.spring_mvc_cobranca.controller;
 
+import com.algaworks.spring_mvc_cobranca.model.StatusTitulo;
 import com.algaworks.spring_mvc_cobranca.model.Titulo;
 import com.algaworks.spring_mvc_cobranca.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/titulos")
@@ -16,8 +21,10 @@ public class TituloController {
     private Titulos titulos;
 
     @RequestMapping("/novo")
-    public String novo() {
-        return "CadastroTitulo";
+    public ModelAndView novo() {
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+
+        return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -27,5 +34,10 @@ public class TituloController {
         mv.addObject("mensagem", "Título salvo com sucesso!");
 
         return mv;
+    }
+
+    @ModelAttribute("todosStatusTitulo")
+    public List<StatusTitulo> todosStatusTitulo() {
+        return Arrays.asList(StatusTitulo.values());
     }
 }
